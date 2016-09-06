1e82e4d06a863d9f6f7a5888faeacdf70cc56aaf
Matt Pindell <pindell.m@gmail.com>
2016-09-05 22:14:08 -0400

    Add App, Header, Order, Inventory Components

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,59 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 
-/*
-  StorePicker
-  This will let us make <StorePicker/>
-*/
-
-let StorePicker = React.createClass({
-
+// App: <App/>
+let App = React.createClass({
   render : () => {
     return (
-      <p>Hello</p>
+      <div className='catch-of-the-day'>
+        <div className='menu'>
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
     )
   }
-
 });
 
-ReactDOM.render(<StorePicker/>, document.getElementById('main'))
+// Header: <Header/>
+let Header = React.createClass({
+  render : () => {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+// Order: <Order/>
+let Order = React.createClass({
+  render : () => {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+// Inventory: <Inventory/>
+let Inventory = React.createClass({
+  render : () => {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+// StorePicker: <StorePicker/>
+let StorePicker = React.createClass({
+  render : () => {
+    return (
+      <form className='store-selector'>
+        <h2>Please Enter A Store</h2>
+        <input type='text' ref='storeId' required />
+        <input type='Submit' />
+      </form>
+    )
+  }
+});
+
+ReactDOM.render(<App/>, document.getElementById('main'))

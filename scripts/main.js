eafae0be533a848c565d9f693a439351560d6b08
Matt Pindell <pindell.m@gmail.com>
2016-09-20 21:58:11 -0400

    Add createFish function to AddFishForm

--- scripts/main.js
+++ scripts/main.js
@@ -28,18 +28,31 @@ var App = React.createClass({
 // Add Fish Form: <AddFishForm/>
 
 var AddFishForm = React.createClass({
+  createFish : function(event) {
+    // stop form from submitting
+    event.preventDefault();
+    // Take form data and create obj
+    var fish = {
+      name : this.refs.name.value,
+      price : this.refs.price.value,
+      status : this.refs.status.value,
+      desc : this.refs.desc.value,
+      image : this.refs.image.value
+    }
+    // Add fish to App State
+  },
   render : function() {
     return (
-      <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
-        <input type="text" ref="name" placeholder="Fish Name"/>
-        <input type="text" ref="price" placeholder="Fish Price" />
-        <select ref="status">
-          <option value="available">Fresh!</option>
-          <option value="unavailable">Sold Out!</option>
+      <form className='fish-edit' ref='fishForm' onSubmit={this.createFish}>
+        <input type='text' ref='name' placeholder='Fish Name'/>
+        <input type='text' ref='price' placeholder='Fish Price' />
+        <select ref='status'>
+          <option value='available'>Fresh!</option>
+          <option value='unavailable'>Sold Out!</option>
         </select>
-        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
-        <input type="text" ref="image" placeholder="URL to Image" />
-        <button type="submit">+ Add Item </button>
+        <textarea type='text' ref='desc' placeholder='Desc'></textarea>
+        <input type='text' ref='image' placeholder='URL to Image' />
+        <button type='submit'>+ Add Item </button>
       </form>
     )
   }

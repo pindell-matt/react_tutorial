287e1203f9f45cf2eaee11bdd9bc152a9eea0900
Matt Pindell <pindell.m@gmail.com>
2016-09-21 21:00:23 -0400

    Add loadSampleFishes func/button

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,11 @@ var App = React.createClass({
     // set state
     this.setState({ fishes : this.state.fishes });
   },
+  loadSamples : function() {
+    this.setState({
+      fishes : require('./sample-fishes')
+    });
+  },
   render : function() {
     return (
       <div className='catch-of-the-day'>
@@ -32,7 +37,7 @@ var App = React.createClass({
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
@@ -109,6 +114,7 @@ var Inventory = React.createClass({
       <div>
         <h2>Inventory</h2>
         <AddFishForm {...this.props} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
